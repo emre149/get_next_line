8e507092b6fb94a6db308f07c2e5e0fdd0351bcf
emre149 <ededemog@student.42.fr>
2024-01-09 14:07:02 +0100

    dd

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 14:03:31 by ededemog          #+#    #+#             */
-/*   Updated: 2024/01/08 16:20:45 by ededemog         ###   ########.fr       */
+/*   Updated: 2024/01/09 12:29:30 by ededemog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ void	add(t_list **list, char *buf)
 		*list = new_element;
 	else
 		last_element->next = new_element;
-
 	new_element->str_buf = buf;
 	new_element->next = NULL;
 }
@@ -49,10 +48,8 @@ void	list_creation(t_list **list, int fd)
 			return ;
 		}
 		buf[char_read] = '\0';
-
 		add(list, buf);
 	}
-
 }
 
 char	*get_line(t_list *list)
@@ -78,11 +75,8 @@ char	*get_next_line(int fd)
 	// cas d'erreur de lecture du fd
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
 		return (NULL);
-
 	list_creation(&list, fd);
-
 	if (list == NULL)
 		return (list);
-
 	next_line = get_line_of(list);
 }
