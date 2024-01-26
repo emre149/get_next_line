d7c7a470d10cd4b7c009edbbd02d52e683622830
emre149 <ededemog@student.42.fr>
2024-01-26 07:40:45 +0100

    .h oublier mdr

---
+++ get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 04:00:05 by ededemog          #+#    #+#             */
+/*   Updated: 2024/01/26 07:10:55 by ededemog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+
+void	read_to_stash(int fd, t_list **stash, int *already_read_ptr);
+
+t_list	*ft_lstlast(t_list *list);
+
+int		newline(t_list *stash);
+
+#endif
